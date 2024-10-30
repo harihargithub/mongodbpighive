fa1530ae4a00df4669005d37384730571cb94565
harihargithub <121367488+harihargithub@users.noreply.github.com>
2024-10-30 18:13:09 +0530

    c@301024 1812 demoproj1 n 2 added

---
+++ script2.js
@@ -0,0 +1,5 @@
+// JavaScript functionality
+document.getElementById('actionButton').addEventListener('click', function () {
+  document.getElementById('output').textContent =
+    'Button clicked! JavaScript is working!';
+});
